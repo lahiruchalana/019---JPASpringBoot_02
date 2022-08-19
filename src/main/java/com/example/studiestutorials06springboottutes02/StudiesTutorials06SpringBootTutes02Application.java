5db2eca169c6b2fbc4ccf7087c3f5306c1fa474e
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-08-19 19:12:18 +0530

    first commit

---
+++ src/main/java/com/example/studiestutorials06springboottutes02/StudiesTutorials06SpringBootTutes02Application.java
@@ -0,0 +1,13 @@
+package com.example.studiestutorials06springboottutes02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesTutorials06SpringBootTutes02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudiesTutorials06SpringBootTutes02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studiestutorials06springboottutes02/StudiesTutorials06SpringBootTutes02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studiestutorials06springboottutes02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudiesTutorials06SpringBootTutes02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
