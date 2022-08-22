2a863e852e80c1b75aa182570ec9f23795e301a1
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-08-22 18:05:24 +0530

    get course data and fixed courses array data posting

--- src/main/java/com/example/studiestutorials06springboottutes02/controller/StudentController.java
+++ src/main/java/com/example/studiestutorials06springboottutes02/controller/StudentController.java
@@ -4,10 +4,9 @@ import com.example.studiestutorials06springboottutes02.entity.Student;
 import com.example.studiestutorials06springboottutes02.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/student")
@@ -25,5 +24,10 @@ public class StudentController {
     ) {
         studentService.createNewStudent(student);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(studentService.getAllStudents(), HttpStatus.OK);
     }
 }
--- src/main/java/com/example/studiestutorials06springboottutes02/service/StudentService.java
+++ src/main/java/com/example/studiestutorials06springboottutes02/service/StudentService.java
@@ -4,6 +4,8 @@ import com.example.studiestutorials06springboottutes02.entity.Student;
 import com.example.studiestutorials06springboottutes02.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class StudentService {
 
@@ -15,5 +17,9 @@ public class StudentService {
 
     public void createNewStudent(Student student) {
         studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 }
